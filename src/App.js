52bdec5fd6072005b1edb21f787c20b95b0faa9e
Diva Juni Artha <divajuni0406@gmail.com>
2022-09-24 21:40:53 +0800

    make user login page that user be able to login with their username and password, but user be able to login via facebook and google too

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-import * as React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from "./pages/Login";
-import { Provider } from "react-redux";
-import store from "./store/store";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import { Homepage, Test, ProductDetail, ProductList, OrderList } from './pages/index'
+import { Provider } from 'react-redux'
+import store from './store/store'
 function App() {
   return (
     <Provider store={store}>
       <BrowserRouter>
         <Routes>
-          <Route path="login" element={<Login />} />
+          <Route path="/" element={<Homepage />} />
+          <Route path="/test" element={<Test />} />
+          <Route path="/products" element={<ProductList />} />
+          <Route path="/products/:id" element={<ProductDetail />} />
+          <Route path="/order-list" element={<OrderList />} />
         </Routes>
       </BrowserRouter>
     </Provider>
