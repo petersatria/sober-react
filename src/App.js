d8dba397a1246f37d2788717c91e82a421ebb75e
Peter Satria <cahyaspeter@gmail.com>
2022-09-25 19:13:04 +0700

    update on branch and product

--- src/App.js
+++ src/App.js
@@ -15,31 +15,31 @@ import Header from './components/Layouts/Header'
 
 function App() {
   return (
-      <BrowserRouter>
-        <Header/>
-        <Routes>
-          <Route path="/" element={<Navigate to="/main/home" />} />
-          <Route path="/main/*" element={<Mainpage />}>
-              <Route path="home" element={<Homepage />} />
-              <Route path="shop" element={<h1>shop</h1>} />
-              <Route path="features" element={<h1>features</h1>} />
-              <Route path="pages" element={<h1>pages</h1>} />
-              <Route path="blog" element={<h1>blog</h1>} />
-          </Route>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Navigate to="/main/home" />} />
+        <Route path="/main/*" element={<Mainpage />}>
+          <Route path="home" element={<Homepage />} />
+          <Route path="shop" element={<h1>shop</h1>} />
+          <Route path="features" element={<h1>features</h1>} />
+          <Route path="pages" element={<h1>pages</h1>} />
+          <Route path="blog" element={<h1>blog</h1>} />
+        </Route>
 
-          <Route path="/signup" element={<SignupPage />} />
+        <Route path="/signup" element={<SignupPage />} />
 
-          <Route path="/admin" element={<Dashboard />}>
-              <Route path="edit/:productId" element={<FormDashboard />} />
-          </Route>
-          <Route path="/test" element={<Test />} />
-          <Route path="/products" element={<ProductList />} />
-          <Route path="/products/:id" element={<ProductDetail />} />
-          <Route path="/order-list" element={<OrderList />} />
-          <Route path="/cart" element={<Cart/>} />
-        </Routes>
-      </BrowserRouter>
+        <Route path="/admin" element={<Dashboard />}>
+          <Route path="edit/:productId" element={<FormDashboard />} />
+        </Route>
+        <Route path="/test" element={<Test />} />
+        <Route path="/products" element={<ProductList />} />
+        <Route path="/products/:id" element={<ProductDetail />} />
+        <Route path="/order-list/:userId/" element={<OrderList />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
+    </BrowserRouter>
   );
-  }
+}
 
 export default App;
